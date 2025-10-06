2a116d86a6c67dd9806eb3dbcaba10aff6f2940b
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2025-10-06 16:59:39 -0300

    [add] cors middleware for fast api

--- materialfastapi/main.py
+++ materialfastapi/main.py
@@ -2,13 +2,25 @@
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.scraping import Scraping
 from core.material import MaterialList
 
 app = FastAPI(title="Agile Inventory API")
 scraper = Scraping("user", "password")
-
+origins = [
+    "http://localhost:8090",
+    "http://10.48.0.188:8090",
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
