01ff83a6189d8a8b8888d3af34d2c54132bb7c00
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2025-10-07 07:58:53 -0300

    [remove] unused imports

--- agileinventory/src/main/java/br/agile/inventory/agileinventory/controller/ProductionOrderController.java
+++ agileinventory/src/main/java/br/agile/inventory/agileinventory/controller/ProductionOrderController.java
@@ -2,24 +2,15 @@ package br.agile.inventory.agileinventory.controller;
 
 
 import java.util.List;
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import br.agile.inventory.agileinventory.repository.ProductionOrderRepository;
 import br.agile.inventory.agileinventory.model.ProductionOrder;
 
 @Controller
 public class ProductionOrderController {
-
-    private final RestTemplate restTemplate = new RestTemplate();
 
     private final ProductionOrderRepository productionOrderRepository;
     public ProductionOrderController(ProductionOrderRepository productionOrderRepository) {
