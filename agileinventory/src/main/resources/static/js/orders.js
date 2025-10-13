ecb276d11e2876304e4eb5bacaf4070835bd209d
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2025-10-13 10:56:24 -0300

    [feat] button to open camera to read barcode

--- agileinventory/src/main/resources/static/js/orders.js
+++ agileinventory/src/main/resources/static/js/orders.js
@@ -1,5 +1,10 @@
-const searchButton = document.getElementById("add-order-btn");
+const openModalBtn = document.getElementById("add-order-btn");
+const searchBtn = document.getElementById("searchBtn");
+const addOrderModal = document.getElementById("add-order-modal");
 const buttons = document.querySelectorAll("[data-dropdown]");
+const searchInput = document.getElementById("searchInput");
+const scanBtn = document.getElementById("scanBtn");
+const camera = document.getElementById("camera");
 
 function toggleDropdown(dropId) {
     const dropdown = document.getElementById(dropId);
@@ -17,19 +22,24 @@ function toggleDropdown(dropId) {
     }
 }
 
-
 function openModal() {
-    const addOrderModal = document.getElementById("add-order-modal");
     addOrderModal.classList.remove("hidden");
 }
 
 function closeModal() {
-    const addOrderModal = document.getElementById("add-order-modal");
-    const searchInput = document.getElementById("searchInput");
     searchInput.value = "";
     addOrderModal.classList.add("hidden");
 }
 
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault
+  if (searchInput.value === ''){
+    document.getElementById("warning").classList.remove("hidden");
+    return
+  } else {
+    document.getElementById("warning").classList.add("hidden");
+  }
+});
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
@@ -38,6 +48,10 @@ document.addEventListener("keydown", (e) => {
       modal.classList.add("hidden"); // esconde qualquer modal aberto
     });
   }
+});
+
+openModalBtn.addEventListener("click", () => {
+    openModal();
 });
 
 // Orders Dropdown Buttons
@@ -52,4 +66,52 @@ buttons.forEach(button => {
       dropdown.classList.toggle("hidden");
       icon.classList.toggle("rotate-180");
     });
+});
+
+// Barcode
+const config = {
+  inputStream: {
+    name: "Live",
+    type: "LiveStream",
+    target: camera,
+    constraints: {
+      facingMode: "environment",
+    },
+  },
+  decoder: {
+    readers: ["code_128_reader", "ean_reader", "upc_reader", "ean_8_reader"],
+  },
+  locate: true,
+};
+
+function stopScanner() {
+  Quagga.stop();
+  camera.classList.add("hidden");
+}
+
+scanBtn.addEventListener("click", () => {
+
+  Quagga.init(config, (err) => {
+    if (err) {
+      console.error("Erro ao inicializar o QuaggaJS:", err);
+      alert("Erro ao acessar a câmera. Verifique as permissões.");
+      stopScanner();
+      return;
+    }
+    Quagga.start();
+  });
+});
+
+Quagga.onDetected((result) => {
+  const code = result.codeResult.code;
+  console.log("Código detectado:", code);
+  searchInput.value = code;
+  stopScanner(); 
+});
+
+const closeModalBtn = document.getElementById("close-modal");
+closeModalBtn.addEventListener('click', () => {
+    if (Quagga.initialized) {
+        stopScanner();
+    }
 });
